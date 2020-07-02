48158fd271bf9a1722b3a8b6874ed840dcb239d3
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-02 13:01:10 +0900

    all columns to devise

--- db/migrate/20200701142554_devise_create_users.rb
+++ db/migrate/20200701142554_devise_create_users.rb
@@ -7,6 +7,21 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_furi, null: false
+      t.string :first_name_furi, null: false
+      t.date :birth, null: false
+      t.string :send_last_name, null: false
+      t.string :send_first_name, null: false
+      t.string :send_last_name_furi, null: false
+      t.string :send_first_name_furi, null: false
+      t.integer :post, null: false
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :block, null: false
+      t.string :building
+      t.string :phone
 
       ## Recoverable
       t.string   :reset_password_token
